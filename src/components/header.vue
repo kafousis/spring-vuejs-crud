fbb29268aafac2fc7626fc41642eaa0ab4bfca0d
Giannis Kafousis <g.kafousis@gmail.com>
2022-08-06 00:58:30 +0300

    toolbar buttons

--- src/components/header.vue
+++ src/components/header.vue
@@ -6,18 +6,47 @@
     </template>
 
     <template #end>
-      <Button
-        icon="pi pi-info"
-        class="p-button-rounded p-button-outlined mr-4"
-      />
+      <router-link to="/books" custom v-slot="{ navigate }">
+        <Button
+          icon="pi pi-home"
+          class="p-button-rounded p-button-outlined mr-4"
+          @click="navigate"
+        />
+      </router-link>
+      <router-link to="/about" custom v-slot="{ navigate }">
+        <Button
+          icon="pi pi-info"
+          class="p-button-rounded p-button-outlined"
+          @click="navigate"
+        />
+      </router-link>
+
+      <i class="pi pi-ellipsis-v p-toolbar-separator mx-4" />
+
       <Button
         icon="pi pi-github"
         class="p-button-rounded p-button-outlined mr-4"
+        @click="githubClicked"
       />
       <Button
         icon="pi pi-linkedin"
         class="p-button-rounded p-button-outlined mr-4"
+        @click="linkedinClicked"
       />
     </template>
   </Toolbar>
 </template>
+
+<script>
+export default {
+  name: "app",
+  methods: {
+    githubClicked() {
+      window.open("https://github.com/kafousis/spring-vuejs-crud");
+    },
+    linkedinClicked() {
+      window.open("https://www.linkedin.com/in/gkafousis/");
+    },
+  },
+};
+</script>
