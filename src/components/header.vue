85b722b88de0783f5d59f1a82d0db9a34c6b9ff1
Giannis Kafousis <g.kafousis@gmail.com>
2022-08-05 18:20:31 +0300

    top toolbar design

---
+++ src/components/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <Toolbar>
+    <template #start>
+      <img alt="Logo" src="../assets/logo.png" width="40" height="40" />
+      <h1 class="text-primary text-2xl font-bold ml-5">SPRING - VUEJS CRUD</h1>
+    </template>
+
+    <template #end>
+      <Button
+        icon="pi pi-info"
+        class="p-button-rounded p-button-outlined mr-4"
+      />
+      <Button
+        icon="pi pi-github"
+        class="p-button-rounded p-button-outlined mr-4"
+      />
+      <Button
+        icon="pi pi-linkedin"
+        class="p-button-rounded p-button-outlined mr-4"
+      />
+    </template>
+  </Toolbar>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
+// https://www.primefaces.org/primevue/setup
+
+import 'primevue/resources/themes/saga-blue/theme.css'  //theme
+import 'primevue/resources/primevue.min.css'            //core css
+import 'primeicons/primeicons.css'                      //icons
+import 'primeflex/primeflex.css';                       // primeflex
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import PrimeVue from 'primevue/config';
+import Toolbar from 'primevue/toolbar';
+import Button from 'primevue/button';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
-app.use(PrimeVue)
+
+// ripple is an optional animation for the supported components
+// Outlined & Filled Input Styles, add {inputStyle: 'filled'}
+app.use(PrimeVue, { ripple: true })
+
+app.component('Toolbar', Toolbar);
+app.component('Button', Button);
+
 
 app.mount('#app');
